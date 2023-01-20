ad0de9a2f7c37b53a46a14efa6b47b1db5903ac6
Stas-0k <brickbybrick15@gmail.com>
2023-01-20 23:29:23 +0200

    commit
    commit

--- src/components/feedback-options/FeedBackOptions.js
+++ src/components/feedback-options/FeedBackOptions.js
@@ -1,7 +1,7 @@
 const FeedBackOptions = ({options, onLeaveFeedback}) => { 
     const object = Object.keys(options)
     return (object.map(item =>  
-            <button type='button' onClick={onLeaveFeedback}>{[item]}</button>
+            <button type='button' onClick={() => onLeaveFeedback(item)} key={item}>{[item]}</button>
     )        
     )
 }
--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -2,7 +2,10 @@ import PropTypes from 'prop-types';
 import cont from './section.module.css';
 
 function Section({title, children }) {
-  return <section className={cont.container}><h1>{title}</h1> <div>{children}</div></section>;
+  return <section className={cont.container}>
+    <h1>{title}</h1>
+    <div>{children}</div>
+  </section>;
 }
 
 Section.propTypes = {
